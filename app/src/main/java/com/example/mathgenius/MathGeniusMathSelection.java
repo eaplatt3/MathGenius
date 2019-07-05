f570f38ab444cdf53b21be601c4584fd9e4e57db
Earl Platt III <36335481+eaplatt3@users.noreply.github.com>
2019-07-05 14:21:20 -0400

    Working on second pasge

---
+++ app/src/main/java/com/example/mathgenius/MathGeniusMathSelection.java
@@ -0,0 +1,13 @@
+package com.example.mathgenius;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MathGeniusMathSelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_math_genius_math_selection);
+    }
+}
