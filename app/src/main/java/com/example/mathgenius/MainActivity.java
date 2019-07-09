29fc094212843ad4878bbf3c058686ef1b311962
Earl Platt III <36335481+eaplatt3@users.noreply.github.com>
2019-07-09 13:00:40 -0400

    Added action to exit and start button, Worked a scrollable activity

--- app/src/main/java/com/example/mathgenius/MainActivity.java
+++ app/src/main/java/com/example/mathgenius/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.mathgenius;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    //XMl File Objects
+    @BindView(R.id.startButton)
+    Button startBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        //Method to selection screen
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MathGeniusMathSelection.class));
+                finish();
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/mathgenius/MathGeniusMathSelection.java
+++ app/src/main/java/com/example/mathgenius/MathGeniusMathSelection.java
@@ -2,12 +2,69 @@ package com.example.mathgenius;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
 
 public class MathGeniusMathSelection extends AppCompatActivity {
+
+    //XMl File Objects
+    @BindView(R.id.addButton)
+    Button addBtn;
+    @BindView(R.id.subButton)
+    Button subBtn;
+    @BindView(R.id.multiButton)
+    Button multiBtn;
+    @BindView(R.id.divideButton)
+    Button divideBtn;
+    @BindView(R.id.exitButton)
+    Button exitBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_math_genius_math_selection);
+        ButterKnife.bind(this);
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        subBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        multiBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        divideBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        exitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.exit(1);
+
+            }
+        });
     }
 }
